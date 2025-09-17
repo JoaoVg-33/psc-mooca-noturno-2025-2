ca7e2f56a6d5f9dd811a051f4f68f50f68775151
João Victor Gabriel Ferreira Rodrigues <825149732@ulife.com.br>
2025-09-16 21:39:20 -0300

    criando calculadora aula 1

---
+++ calculadora.java
@@ -0,0 +1,27 @@
+public class calculadora{
+
+    public void soma(int a, int b){
+        int resultado;
+        resultado = a + b;
+        return resultado;
+    }
+
+    public void subtracao(int a, int b){
+        int resultado;
+        resultado = a - b;
+        return resultado;
+    }
+
+    public void multiplicacao(int a, int b){
+        int resultado;
+        resultado = a * b;
+        return resultado;
+    }
+
+    public void divisao(int a, int b){
+        int resultado;
+        resultado = a / b;
+        return resultado;
+    }
+}
+
