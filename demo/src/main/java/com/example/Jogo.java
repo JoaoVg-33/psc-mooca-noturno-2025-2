eec46959eceafc8611a528be17d18e383f3ea3d3
João Victor Gabriel <825149732@ulife.com.br>
2025-09-16 21:07:12 -0300

    aula 3 poo

--- demo/src/main/java/com/example/Jogo.java
+++ demo/src/main/java/com/example/Jogo.java
@@ -4,17 +4,22 @@ package com.example;
 public class Jogo {
 
     public static void main(String[] args) {
-        Personagem cacador = new Personagem();
-        cacador.nome = "João";
+        Personagem cacador = new Personagem("João");
+        Personagem soneca = new Personagem("Danilo");
 
 
         while(true){
         cacador.cacar();
+        soneca.dormir();
         cacador.comer();
+        soneca.dormir();
         cacador.dormir();
+        soneca.dormir();
         cacador.cacar();
         cacador.cacar();
+        soneca.comer();
         cacador.cacar();
+        soneca.cacar();
         System.out.println("========================");
             try {
                 Thread.sleep(2000);
