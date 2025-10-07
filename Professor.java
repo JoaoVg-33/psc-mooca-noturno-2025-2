d226ae2338846af366ad0b7a6b775b68730f5c30
João Victor Gabriel <825149732@ulife.com.br>
2025-10-06 21:07:15 -0300

    Add files via upload

---
+++ Professor.java
@@ -0,0 +1,54 @@
+public class professor {
+    
+    public class Professor {
+        private String matricula;
+        private String nome;
+        private String email;
+    
+        public Professor(String matricula, String nome, String email) {
+            this.matricula = matricula;
+            this.nome = nome;
+            this.email = email;
+        }
+    
+        // Getters e Setters
+        public String getMatricula() {
+            return matricula;
+        }
+    
+        public void setMatricula(String matricula) {
+            this.matricula = matricula;
+        }
+    
+        public String getNome() {
+            return nome;
+        }
+    
+        public void setNome(String nome) {
+            this.nome = nome;
+        }
+    
+        public String getEmail() {
+            return email;
+        }
+    
+        public void setEmail(String email) {
+            this.email = email;
+        }
+    
+        // Métodos adicionais
+        public void lancarNota(aluno aluno, double nota) {
+            aluno.setNota(nota);
+            System.out.println("Nota lançada para " + aluno.getNome() + ": " + nota);
+        }
+    
+        public void visualizarNota(aluno aluno) {
+            System.out.println("Nota do aluno " + aluno.getNome() + ": " + aluno.getNota());
+        }
+    
+        public void visualizarAluno(aluno aluno) {
+            System.out.println("Dados do aluno:");
+            System.out.println(aluno);
+        }
+    }
+}    
